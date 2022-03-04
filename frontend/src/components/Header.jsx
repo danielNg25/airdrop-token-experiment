19390212bc18f0b3f4dec63c63d6f41f3cfdc53d
ndtr2000 <53716352+ndtr2000@users.noreply.github.com>
2022-03-05 00:19:32 +0700

    connect Metamask

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,48 @@
 import React from 'react'
 import { Button } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.css';
+import { useState } from 'react';
+import { ethers } from 'ethers';
 export default function Header() {
+
+
+  const [walletAddress, setWalletAddress] = useState("");
+  const [provider, setProvider] = useState("");
+  async function requestAccount() {
+    console.log('Requesting account...');
+    if(window.ethereum) {
+      console.log('detected');
+
+      try {
+        const accounts = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        setWalletAddress(accounts[0]);
+        console.log(accounts[0]);
+      } catch (error) {
+        console.log('Error connecting...');
+      }
+
+    } else {
+      alert('Meta Mask not detected');
+    }
+  }
+
+  // Create a provider to interact with a smart contract
+  async function connectWallet() {
+    if(typeof window.ethereum !== 'undefined') {
+      await requestAccount();
+      const ethersProvider = new ethers.providers.Web3Provider(window.ethereum);
+      await ethersProvider.send("eth_requestAccounts", []);
+      const signer = ethersProvider.getSigner();
+      setProvider(ethersProvider);
+      console.log(signer);
+    }
+  }
   return (
     <div className='Header'>
         <div className='Header-title '>AIR DROP</div>
-        <Button variant="danger" className='Connect-btn'>Connect</Button>
+        <Button variant="danger" className='Connect-btn' onClick={connectWallet}>Connect</Button>
     </div>
   )
 }
