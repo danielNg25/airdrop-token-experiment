feeecd0e5e49cc1a94fabd9272c9449a0a02a7be
ndtr2000 <53716352+ndtr2000@users.noreply.github.com>
2022-03-05 23:56:32 +0700

    update claim route

--- frontend/src/components/Claim.jsx
+++ frontend/src/components/Claim.jsx
@@ -3,18 +3,23 @@ import { useSelector, useDispatch } from "react-redux";
 import { addressSelector, signerSelector, tokenSelector } from "../app/reducer/authSlice";
 import { Button } from "react-bootstrap";
 import axios from "axios";
+import { ethers } from "ethers";
 import "bootstrap/dist/css/bootstrap.css";
+import AIRDROP from "../abi/AirDrop.json";
 export default function Claim() {
     const address = useSelector(addressSelector);
     const signer = useSelector(signerSelector);
     const token = useSelector(tokenSelector);
     const dispatch = useDispatch();
     const [currentAmount, setCurrentAmount] = useState(0);
+    const [isClaimed, setIsClaimed] = useState(0);
     useEffect(() => {
         const getCurrentAmount = async () => {
             try {
                 const response = await axios.get("/amount/" + address);
                 setCurrentAmount(response.data.amount);
+                setIsClaimed(response.data.isClaimed);
+                console.log(response.data.isClaimed);
             } catch (err) {
                 console.log(err.response.data);
             }
@@ -24,7 +29,7 @@ export default function Claim() {
 
     const handleClaim = async () => {
         try {
-            const response = await axios.post(
+            const responsePost = await axios.post(
                 "/amount/claim",
                 {
                     address: address,
@@ -35,17 +40,38 @@ export default function Claim() {
                     },
                 }
             );
-            console.log(response.data)
+            console.log(responsePost.data);
+            const airDropContract = new ethers.Contract("0x4308251514f5215504ea644A936167adDe994d91", AIRDROP, signer);
+            const res = await airDropContract.redeem(address, currentAmount, responsePost.data);
+            if (res) {
+                const responsePut = await axios.put(
+                    "/amount/claim",
+                    { address: address },
+                    {
+                        headers: {
+                            "x-access-token": token,
+                        },
+                    }
+                );
+            }
         } catch (err) {
-            console.log(err.response.data);
+            console.log(err);
+            alert(err.response.data);
         }
     };
 
     return (
         <div className="Body-content">
-            <Button className="Main-btn" variant="success" onClick={handleClaim}>
-                Claim
-            </Button>
+            {isClaimed ? (
+                <Button className="Main-btn" variant="secondary" disabled>
+                    Claimed
+                </Button>
+            ) : (
+                <Button className="Main-btn" variant="success" onClick={handleClaim}>
+                    Claim
+                </Button>
+            )}
+
             <div className="Amount-text">Your current amount: {currentAmount} NDT</div>
         </div>
     );
